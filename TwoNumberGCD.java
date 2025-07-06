b187eb7dd732009052760816055def27e6b273c1
ashishrajput35 <ashishrajput35cs@gmail.com>
2025-07-07 00:47:04 +0530

    added new problems

---
+++ TwoNumberGCD.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TwoNumberGCD{
+
+    public static void main(String[] arg){
+
+        System.out.println("Find the GCD of two numbers");
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
